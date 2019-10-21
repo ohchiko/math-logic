582053fda646b65871d972f668855cff135557dd
Gregorio Chiko Putra <chiko150@outlook.com>
2019-10-21 11:19:34 +0700

    Changed the title display on landing page to be more visible

--- src/js/components/landing.js
+++ src/js/components/landing.js
@@ -2,25 +2,29 @@
 import Header from './_header';
 
 var app = {
-    view: () => {
-        return m('.container.flex', [
-            m(Header),
-            m('.content', {
-                style: { backgroundColor: randomizeColor() }
-            }, [
-                m('.title', [
-                    m('.title-text', 'LEARN'),
-                    m('.title-text', 'MATH LOGICS'),
-                    m('.title-text.invert', 'THROUGH'),
-                    m('.title-text.invert', 'WEB APPLICATION')
-                ]),
-                m('.desc',
-                    m('span', 'Pelajari logika matematika\
-                                serta lakukan penghitungan\
-                                melalui aplikasi ini.'))
-            ])
-        ]);
-    }
+  view: () => {
+    return m('.container.flex', [
+      m(Header),
+      m('.content', {
+        style: { backgroundColor: randomizeColor() }
+      }, [
+        m('.title', [
+          m('.title-text',
+            m('span', 'LEARN')),
+          m('.title-text',
+            m('span', 'MATH LOGICS')),
+          m('.title-text.invert',
+            m('span', 'THROUGH')),
+          m('.title-text.invert',
+            m('span', 'WEB APPLICATION'))
+        ]),
+        m('.desc',
+          m('span', 'Pelajari logika matematika\
+                    lakukan penghitungan\
+                    melalui aplikasi ini.'))
+      ])
+    ]);
+  }
 };
 
 export default app;
